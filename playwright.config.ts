c5cfeca95f7aa2b277fee056ce3e1eba90c18a81
unknown <florezvasquezjuanmiguel@gmail.com>
2026-05-24 16:11:26 -0500

    Fix: update projects config

--- playwright.config.ts
+++ playwright.config.ts
@@ -51,7 +51,11 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'] ,
+      launchOptions: {
+          args: ['--disable-blink-features=AutomationControlled']
+        }
+      },
     },
 
     {
